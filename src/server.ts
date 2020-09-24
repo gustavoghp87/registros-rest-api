9c87809caac5e0eb3b1fb9fb34a36e287e060df9
gustavoghp87 <ghp.2120@gmail.com>
2020-09-23 23:33:44 -0300

    scripts

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,13 @@
 //const express = require('express');
 import express from 'express';
 import path from 'path';
-import morgan from 'morgan';
+// import morgan from 'morgan';
 const app = express();
 
 const port = process.env.PORT || 4005;
 
 // middlewares
-app.use(morgan('dev'));
+// app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 

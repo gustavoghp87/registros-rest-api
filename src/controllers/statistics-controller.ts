5441502e839efd904305cbed88e57ebf92b0e983
Gustavo Hernán Perrupato <ghp.2120@gmail.com>
2021-05-24 22:24:38 -0300

    global stat

--- src/controllers/statistics-controller.ts
+++ src/controllers/statistics-controller.ts
@@ -4,6 +4,8 @@ import * as territoryServices from '../services/territory-services'
 export const router = express.Router()
 
 router.post('/', async (req, res) => {
-    let datos2 = await territoryServices.getLocalStatistics(req.body.token, req.body.territorio)
+    let data = await territoryServices.getLocalStatistics(req.body.token, req.body.territorio)
+    if (!data) return null
+    const datos2 = {...data, territorio: req.body.territorio}
     res.json(datos2)
 })

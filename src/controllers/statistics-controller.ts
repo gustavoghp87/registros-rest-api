35c6c11b4dc70c363f2a0e5395068fd9aac4d2a0
Gustavo Hernán Perrupato <ghp.2120@gmail.com>
2021-05-24 22:15:09 -0300

    statistics controller by territory services

--- src/controllers/statistics-controller.ts
+++ src/controllers/statistics-controller.ts
@@ -1,10 +1,9 @@
 import express from 'express'
+import * as territoryServices from '../services/territory-services'
 
 export const router = express.Router()
 
 router.post('/', async (req, res) => {
-    let datos2 = await require('../graphql/queries').getLocalStatistics(
-        "", { token: req.body.token, territorio: req.body.territorio }
-    )
+    let datos2 = await territoryServices.getLocalStatistics(req.body.token, req.body.territorio)
     res.json(datos2)
 })

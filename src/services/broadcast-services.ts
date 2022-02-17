a712c08a212b38d9202725984d0d722408ca38fe
Gustavo Ghp <ghp.21@hotmail.com>
2022-02-16 21:44:20 -0300

    Warning toaster same territory working

--- src/services/broadcast-services.ts
+++ src/services/broadcast-services.ts
@@ -2,7 +2,7 @@ import { Server } from 'socket.io'
 import { domain, server, testingDomain } from '../server'
 import { typeUser } from '../models/user'
 import { typeVivienda } from '../models/vivienda'
-import { typeHTHBuilding, typeHTHHousehold } from '../models/houseToHouse'
+import { typeHTHBuilding } from '../models/houseToHouse'
 
 export const socketConnection = (production: boolean): void => {
     new Server(server, {
@@ -12,20 +12,21 @@ export const socketConnection = (production: boolean): void => {
             credentials: true
         }
     }).on('connection', (socket: any): void => {
-        console.log("NEW SOCKET CONNECTION ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------", socket.id)
+        console.log("NEW SOCKET CONNECTION -------------------------------------------------------------------------", socket.id)
         socket.emit('connection', null);
         socket.on('household: change', (objPackage: any): void => {
+            if (!objPackage) return
             let households: typeVivienda[] = objPackage.households
             const updatedHousehold: typeVivienda = objPackage.updatedHousehold
             const indexOfHousehold: number = objPackage.indexOfHousehold
-            console.log(households.length, updatedHousehold, indexOfHousehold);
+            const userEmail: string = objPackage.userEmail
             if (!households || !updatedHousehold || indexOfHousehold === null) {
                 console.log("\n\nError in socket household: change\n\n");
                 return
             }
             households[indexOfHousehold] = updatedHousehold
-            socket.emit('household: change', households)
-            socket.broadcast.emit('household: change', households)
+            socket.emit('household: change', households, userEmail)
+            socket.broadcast.emit('household: change', households, userEmail)
         })
         socket.on('user: change', (updatedUser: typeUser): void => {
             socket.emit('user: change', updatedUser)

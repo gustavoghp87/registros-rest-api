3477013dbba4297ec1ee363b98f231074967ab24
Gustavo Ghp <ghp.21@hotmail.com>
2022-07-17 12:29:40 -0300

    Geolocation exceptions handlers

--- src/services/geocoding-services.ts
+++ src/services/geocoding-services.ts
@@ -7,30 +7,45 @@ import { typeUser } from '../models/user'
 export const getGeocodingFromAddressService = async (token: string, address: string): Promise<typeCoords|null> => {
     const user: typeUser|null = await getActivatedUserByAccessTokenService(token)
     if (!user) return null
-    const geocoder = NodeGeocoder({
-        provider: 'google',
-        // fetch: customFetchImplementation,
-        apiKey: googleGeocodingAPIKey,
-        formatter: null
-    })
-    const response: NodeGeocoder.Entry[] = await geocoder.geocode({ address, country: 'Argentine' })
-    if (!response || !response[0] || !response[0].latitude || !response[0].longitude) return null
-    return {
-        lat: response[0].latitude,
-        lng: response[0].longitude
+    try {
+        const response: NodeGeocoder.Entry[] = await NodeGeocoder({
+            provider: 'google',
+            // fetch: customFetchImplementation,
+            apiKey: googleGeocodingAPIKey,
+            formatter: null
+        }).geocode({
+            address, country: 'Argentine'
+        })
+        if (!response || !response[0] || !response[0].latitude || !response[0].longitude) return null
+        return {
+            lat: response[0].latitude,
+            lng: response[0].longitude
+        }
+    } catch (error) {
+        console.log(error)
+        // logger
+        return null
     }
 }
 
 export const getGeocodingFromCoordinatesService = async (token: string, coordinates: typeCoords): Promise<string|null> => {
     const user: typeUser|null = await getActivatedUserByAccessTokenService(token)
     if (!user || !coordinates || !coordinates.lat || !coordinates.lng) return null
-    const geocoder = NodeGeocoder({
-        provider: 'google',
-        // fetch: customFetchImplementation,
-        apiKey: googleGeocodingAPIKey,
-        formatter: null
-    })
-    const response: NodeGeocoder.Entry[] = await geocoder.reverse({ lat: coordinates.lat, lon: coordinates.lng })
-    if (!response || !response[0] || !response[0].formattedAddress) return null
-    return response[0].formattedAddress
+    try {
+        const response: NodeGeocoder.Entry[] = await NodeGeocoder({
+            provider: 'google',
+            // fetch: customFetchImplementation,
+            apiKey: googleGeocodingAPIKey,
+            formatter: null
+        }).reverse({
+            lat: coordinates.lat,
+            lon: coordinates.lng
+        })
+        if (!response || !response[0] || !response[0].formattedAddress) return null
+        return response[0].formattedAddress
+    } catch (error) {
+        console.log(error)
+        // logger
+        return null
+    }
 }
